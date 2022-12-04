4b8850ca2af0286fc5ff8e8ffadea2d59a3cdf84
Kyle Trom <ktrom@iastate.edu>
2022-12-04 15:50:43 -0800

    initial commit!

---
+++ src/test/java/com/example/dockerfun/DockerFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerFunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
